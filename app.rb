6dc8904d2db78d306f8db4775d07310b7f5dad86
Anthonyph <anthony.phim@gmail.com>
2019-10-17 21:20:05 +0200

    tellement la hessssssssss

--- app.rb
+++ app.rb
@@ -1,5 +1,40 @@
 require 'bundler'
 Bundler.require
+require_relative 'lib/game.rb'
+require_relative 'lib/show.rb'
 
-# require 'lib/app/scrapper.rb' quand on a besoin de mettre le path exact
-$:.unshift File.expand_path("./../lib/app", __FILE__)
+
+def welcome_message
+puts "#################################################"
+puts "#                                               #"
+puts "#          BIENVENUE DANS LE MORPION            #"
+puts "#                 en RUBY                       #"
+puts "#                                               #"
+puts "#################################################" 
+end 
+
+def ask_name
+
+
+return name_player1
+end
+
+def ask_name2
+
+return name_player2
+end
+
+def perform
+welcome_message
+#name_player1 = ask_name
+#name_player2 = ask_name2
+jeu = Game.new
+puts "êtes vous prêt ?? "
+puts " "
+puts "------- Appuyez sur Entrée pour commencer ----------"
+puts " " 
+gets.chomp 
+jeu.round
+play_again?
+end
+perform 
--- lib/game.rb
+++ lib/game.rb
@@ -2,6 +2,7 @@ require 'pry'
 require_relative 'board'
 #require_relative 'boardcase'
 require_relative 'player'
+require_relative 'show'
 
 class Game < Board
 
@@ -9,9 +10,17 @@ attr_accessor :players #:game_number
 
   def initialize#(game_numb)
     #@game_number = game_numb
+    puts "Joueur 1, quel est ton nom ?"
+    print " > "
+    name_player1 = gets.chomp
+    puts "Joueur 2, quel est ton nom ?"
+    print " > "
+    name_player2 = gets.chomp
+    puts " "
+    puts " merci on va pouvoir commencer ! "
     @boardgame = Board.new(name="jeu")
-    player2 = Player.new(name="Antho", symbol="X")
-    player1 = Player.new(name="leo", symbol="O")
+    player2 = Player.new(name_player2, symbol="X")
+    player1 = Player.new(name_player1, symbol="O")
     @players = [player1, player2]
     return @players
     p @boardgame
@@ -23,15 +32,16 @@ attr_accessor :players #:game_number
   i=0
   z=0
   player = @players[i]
-      until z == 9 do
+      while z != 9 || victory? != false
       puts "#{@players[i].name} choisit une case : "
       input_position = gets.chomp
       @boardgame.find_index_change_state(input_position)
       player= @players[i]
       i=i+1
       z=z+1
-      p @boardgame
+      #p @boardgame
       victory? 
+      #show_board(@boardgame)
         if i==2
           i=0
         end
@@ -59,47 +69,96 @@ attr_accessor :players #:game_number
     def victory?
 
       if @boardgame.cases[0].state == @boardgame.cases[1].state && @boardgame.cases[0].state == @boardgame.cases[2].state && @boardgame.cases[0].state == "X" && @boardgame.cases[0].state != ""   
+        
         puts "You win, #{@players[0].name} !!!!!!!!!!!! Congratulations !!!!!!!!!!!!!!!!!!!!"
+        play_again?
       elsif @boardgame.cases[3].state == @boardgame.cases[4].state && @boardgame.cases[4].state == @boardgame.cases[5].state && @boardgame.cases[4].state == "X" && @boardgame.cases[4].state != "" 
         puts "You win, #{@players[0].name} !!!!!!!!!!!! Congratulations !!!!!!!!!!!!!!!!!!!!"
+        play_again?
       elsif @boardgame.cases[6].state == @boardgame.cases[7].state && @boardgame.cases[8].state == @boardgame.cases[7].state && @boardgame.cases[7].state == "X" && @boardgame.cases[7].state != ""
+        
         puts "You win, #{@players[0].name} !!!!!!!!!!!! Congratulations !!!!!!!!!!!!!!!!!!!!"
+        play_again?
       elsif @boardgame.cases[2].state == @boardgame.cases[4].state && @boardgame.cases[6].state == @boardgame.cases[4].state && @boardgame.cases[4].state == "X" && @boardgame.cases[4].state != ""
+        
         puts "You win, #{@players[0].name} !!!!!!!!!!!! Congratulations !!!!!!!!!!!!!!!!!!!!"
+        play_again?
       elsif @boardgame.cases[0].state == @boardgame.cases[4].state && @boardgame.cases[8].state == @boardgame.cases[4].state && @boardgame.cases[8].state == "X" && @boardgame.cases[8].state != ""
+        
         puts "You win, #{@players[0].name} !!!!!!!!!!!! Congratulations !!!!!!!!!!!!!!!!!!!!"
+        play_again?
       elsif @boardgame.cases[1].state == @boardgame.cases[4].state && @boardgame.cases[4].state == @boardgame.cases[7].state && @boardgame.cases[4].state == "X" && @boardgame.cases[4].state != ""
+        
         puts "You win, #{@players[0].name} !!!!!!!!!!!! Congratulations !!!!!!!!!!!!!!!!!!!!"
+        play_again?
       elsif @boardgame.cases[0].state == @boardgame.cases[3].state && @boardgame.cases[3].state == @boardgame.cases[8].state && @boardgame.cases[8].state == "X" && @boardgame.cases[8].state != ""
+        
         puts "You win, #{@players[0].name} !!!!!!!!!!!! Congratulations !!!!!!!!!!!!!!!!!!!!"
+        play_again?
       elsif @boardgame.cases[2].state == @boardgame.cases[5].state && @boardgame.cases[8].state == @boardgame.cases[2].state && @boardgame.cases[5].state == "X" && @boardgame.cases[5].state != ""
+        
         puts "You win, #{@players[0].name} !!!!!!!!!!!! Congratulations !!!!!!!!!!!!!!!!!!!!"
+        play_again?
       
       
       
       
       elsif @boardgame.cases[0].state == @boardgame.cases[1].state && @boardgame.cases[0].state == @boardgame.cases[2].state && @boardgame.cases[0].state == "O" && @boardgame.cases[0].state != ""   
+        
         puts "You win, #{@players[1].name} !!!!!!!!!!!! Congratulations !!!!!!!!!!!!!!!!!!!!"
+        play_again?
       elsif @boardgame.cases[3].state == @boardgame.cases[4].state && @boardgame.cases[4].state == @boardgame.cases[5].state && @boardgame.cases[4].state == "O" && @boardgame.cases[4].state != "" 
+        
         puts "You win, #{@players[1].name} !!!!!!!!!!!! Congratulations !!!!!!!!!!!!!!!!!!!!"
+        play_again?
       elsif @boardgame.cases[6].state == @boardgame.cases[7].state && @boardgame.cases[8].state == @boardgame.cases[7].state && @boardgame.cases[7].state == "O" && @boardgame.cases[7].state != ""
+        
         puts "You win, #{@players[1].name} !!!!!!!!!!!! Congratulations !!!!!!!!!!!!!!!!!!!!"
+        play_again?
       elsif @boardgame.cases[2].state == @boardgame.cases[4].state && @boardgame.cases[6].state == @boardgame.cases[4].state && @boardgame.cases[4].state == "O" && @boardgame.cases[4].state != ""
+        
         puts "You win, #{@players[1].name} !!!!!!!!!!!! Congratulations !!!!!!!!!!!!!!!!!!!!"
+        play_again?
       elsif @boardgame.cases[0].state == @boardgame.cases[4].state && @boardgame.cases[8].state == @boardgame.cases[4].state && @boardgame.cases[8].state == "O" && @boardgame.cases[8].state != ""
+        
         puts "You win, #{@players[1].name} !!!!!!!!!!!! Congratulations !!!!!!!!!!!!!!!!!!!!"
+        play_again?
       elsif @boardgame.cases[1].state == @boardgame.cases[4].state && @boardgame.cases[4].state == @boardgame.cases[7].state && @boardgame.cases[4].state == "O" && @boardgame.cases[4].state != ""
         puts "You win, #{@players[1].name} !!!!!!!!!!!! Congratulations !!!!!!!!!!!!!!!!!!!!"
+        
+        play_again?
       elsif @boardgame.cases[0].state == @boardgame.cases[3].state && @boardgame.cases[3].state == @boardgame.cases[8].state && @boardgame.cases[8].state == "O" && @boardgame.cases[8].state != ""
         puts "You win, #{@players[1].name} !!!!!!!!!!!! Congratulations !!!!!!!!!!!!!!!!!!!!"
+        
+        play_again?
       elsif @boardgame.cases[2].state == @boardgame.cases[5].state && @boardgame.cases[8].state == @boardgame.cases[2].state && @boardgame.cases[5].state == "O" && @boardgame.cases[5].state != ""
         puts "You win, #{@players[1].name} !!!!!!!!!!!! Congratulations !!!!!!!!!!!!!!!!!!!!"
+        
+        play_again?
+
       
       end
+
     end
   
+    def play_again?
+
+      puts " On rejoue ? "
+      puts "1 - oui"
+      puts "2 - non"
+      print "> "
+      input = gets.chomp 
+      if 
+        input == "1"
+        jeu2 = Game.new 
+        jeu2.round
+      else 
+      puts " à bientôt "
+      exit
+      end
+    end
 end
 
-binding.pry
-jeu =Game.new
-jeu.round
+#binding.pry
+#jeu =Game.new
+#jeu.round
